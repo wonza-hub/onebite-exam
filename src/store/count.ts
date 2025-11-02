3f8a9a79d0bf0c4a82ddec60ff115bd60dc35b92
wonza-hub <wonza8585@gmail.com>
2025-11-02 15:42:52 +0900

    refactor: 카운터 스토어에 zustand 미들웨어 combine 적용

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,29 +1,51 @@
 import { create } from "zustand";
+import { combine } from "zustand/middleware";
 
-type TCounterStore = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
-export const useCounterStore = create<TCounterStore>((set, get) => ({
+// type TCounterStore = {
+//   count: number;
+//   actions: {
+//     increase: () => void;
+//     decrease: () => void;
+//   };
+// };
+// export const useCounterStore = create<TCounterStore>((set, get) => ({
+//   count: 0,
+//   actions: {
+//     increase: () => {
+//       // const count = get().count;
+//       // set({ count: count + 1 });
+//       set((store) => ({
+//         count: store.count + 1,
+//       }));
+//     },
+//     decrease: () => {
+//       set((store) => ({
+//         count: store.count - 1,
+//       }));
+//     },
+//   },
+// }));
+
+// 미들웨어 combine
+const initialState = {
   count: 0,
-  actions: {
-    increase: () => {
-      // const count = get().count;
-      // set({ count: count + 1 });
-      set((store) => ({
-        count: store.count + 1,
-      }));
+};
+export const useCounterStore = create(
+  combine(initialState, (set, get) => ({
+    actions: {
+      increase: () => {
+        set((state) => ({
+          count: state.count + 1,
+        }));
+      },
+      decrease: () => {
+        set((state) => ({
+          count: state.count - 1,
+        }));
+      },
     },
-    decrease: () => {
-      set((store) => ({
-        count: store.count - 1,
-      }));
-    },
-  },
-}));
+  })),
+);
 
 export const useCounterCount = () => {
   return useCounterStore((store) => store.count);
