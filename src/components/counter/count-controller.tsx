980d3939940e17ece90e388bdc286e61ffb03951
wonza-hub <wonza8585@gmail.com>
2025-11-02 15:00:50 +0900

    refactor: 카운터 스토어의 함수를 액션으로 분리

--- src/components/counter/count-controller.tsx
+++ src/components/counter/count-controller.tsx
@@ -3,8 +3,13 @@ import { useCounterStore } from "@/store/count";
 
 export default function CounterController() {
   //   const { increase, decrease } = useCounterStore();
-  const increase = useCounterStore((store) => store.increase);
-  const decrease = useCounterStore((store) => store.decrease);
+
+  // 셀렉터로 스토어에서 일부 상태만 가져오기
+  //   const increase = useCounterStore((store) => store.increase);
+  //   const decrease = useCounterStore((store) => store.decrease);
+
+  // 액션 함수 가져오기
+  const { increase, decrease } = useCounterStore((store) => store.actions);
 
   return (
     <div>
--- src/store/count.ts
+++ src/store/count.ts
@@ -2,21 +2,25 @@ import { create } from "zustand";
 
 type TCounterStore = {
   count: number;
-  increase: () => void;
-  decrease: () => void;
+  actions: {
+    increase: () => void;
+    decrease: () => void;
+  };
 };
 export const useCounterStore = create<TCounterStore>((set, get) => ({
   count: 0,
-  increase: () => {
-    // const count = get().count;
-    // set({ count: count + 1 });
-    set((store) => ({
-      count: store.count + 1,
-    }));
-  },
-  decrease: () => {
-    set((store) => ({
-      count: store.count - 1,
-    }));
+  actions: {
+    increase: () => {
+      // const count = get().count;
+      // set({ count: count + 1 });
+      set((store) => ({
+        count: store.count + 1,
+      }));
+    },
+    decrease: () => {
+      set((store) => ({
+        count: store.count - 1,
+      }));
+    },
   },
 }));
