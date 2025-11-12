e7f25ad070952c1dad18b26132463d21d301e887
wonza-hub <wonza8585@gmail.com>
2025-11-12 23:24:54 +0900

    refactor: 할 일 수정 mutation에 캐시 정규화 적용

--- src/hooks/mutations/use-update-todo-mutation.ts
+++ src/hooks/mutations/use-update-todo-mutation.ts
@@ -12,39 +12,73 @@ export function useUpdateTodoMutation() {
   return useMutation({
     mutationFn: updateTodo,
     onMutate: async (updatedTodo) => {
+      // 낙관적 업데이트
+      // await queryClient.cancelQueries({
+      //   queryKey: QUERY_KEYS.todo.list,
+      // });
+
+      // const prevTodos = queryClient.getQueryData<ITodo[]>(QUERY_KEYS.todo.list);
+      // queryClient.setQueryData<ITodo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+      //   if (!prevTodos) {
+      //     return [];
+      //   }
+
+      //   return prevTodos.map((prevTodo) =>
+      //     prevTodo.id === updatedTodo.id
+      //       ? { ...prevTodo, ...updatedTodo }
+      //       : prevTodo,
+      //   );
+      // });
+
+      // return {
+      //   prevTodos,
+      // };
+
+      // 낙관적 업데이트 + 쿼리 정규화
       await queryClient.cancelQueries({
-        queryKey: QUERY_KEYS.todo.list,
+        queryKey: QUERY_KEYS.todo.detail(updatedTodo.id),
       });
-
-      const prevTodos = queryClient.getQueryData<ITodo[]>(QUERY_KEYS.todo.list);
-      queryClient.setQueryData<ITodo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
-        if (!prevTodos) {
-          return [];
-        }
-
-        return prevTodos.map((prevTodo) =>
-          prevTodo.id === updatedTodo.id
-            ? { ...prevTodo, ...updatedTodo }
-            : prevTodo,
-        );
-      });
+      const prevTodo = queryClient.getQueryData<ITodo>(
+        QUERY_KEYS.todo.detail(updatedTodo.id),
+      );
+      queryClient.setQueryData<ITodo>(
+        QUERY_KEYS.todo.detail(updatedTodo.id),
+        (prevTodo) => {
+          if (!prevTodo) {
+            return;
+          }
+          return {
+            ...prevTodo,
+            ...updatedTodo,
+          };
+        },
+      );
 
       return {
-        prevTodos,
+        prevTodo,
       };
     },
-    onError: (err, variable, context) => {
-      if (context && context.prevTodos) {
-        queryClient.setQueryData<ITodo[]>(
-          QUERY_KEYS.todo.list,
-          context.prevTodos,
+    onError: (error, variable, context) => {
+      // 낙관적 업데이트
+      // if (context && context.prevTodos) {
+      //   queryClient.setQueryData<ITodo[]>(
+      //     QUERY_KEYS.todo.list,
+      //     context.prevTodos,
+      //   );
+      // }
+      // 낙관적 업데이트 + 쿼리 정규화
+      if (context && context.prevTodo) {
+        queryClient.setQueryData<ITodo>(
+          QUERY_KEYS.todo.detail(context.prevTodo.id),
+          context.prevTodo,
         );
       }
     },
-    onSettled: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
-      });
-    },
+    // 쿼리 정규화 시에는 개별 캐시는 disabled이므로 무효화 무의미
+    // onSettled: () => {
+    //   queryClient.invalidateQueries({
+    //     queryKey: QUERY_KEYS.todo.list,
+    //   });
+    // },
   });
 }
