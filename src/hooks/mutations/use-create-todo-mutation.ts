2bcbcab6403ccd72c26660f44621c1dfd6b078ea
wonza-hub <wonza8585@gmail.com>
2025-11-12 23:24:47 +0900

    refactor: 할 일 추가 mutation에 캐시 정규화 적용

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -18,10 +18,25 @@ export function useCreateTodoMutation() {
       // });
 
       // 캐시 직접 수정
-      queryClient.setQueryData<ITodo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
-        if (!prevTodos) return [newTodo];
-        return [...prevTodos, newTodo];
-      });
+      // queryClient.setQueryData<ITodo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+      //   if (!prevTodos) return [newTodo];
+      //   return [...prevTodos, newTodo];
+      // });
+
+      // 캐시 정규화
+      // 개별 아이템에 대한 갱신
+      queryClient.setQueryData<ITodo>(
+        QUERY_KEYS.todo.detail(newTodo.id),
+        newTodo,
+      );
+      // 목록 id 캐시에 대한 갱신
+      queryClient.setQueryData<string[]>(
+        QUERY_KEYS.todo.list,
+        (prevTodoIds) => {
+          if (!prevTodoIds) return [newTodo.id];
+          return [...prevTodoIds, newTodo.id];
+        },
+      );
     },
   });
 }
