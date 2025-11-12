59dbcd4f70cecff561a33894faf094cb0431904a
wonza-hub <wonza8585@gmail.com>
2025-11-12 14:39:37 +0900

    refactor: 할 일 페칭 시 캐시 정규화 적용

--- src/hooks/queries/use-todos-data.ts
+++ src/hooks/queries/use-todos-data.ts
@@ -1,10 +1,22 @@
 import { fetchTodos } from "@/api/fetch-todos";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEYS } from "@/lib/constants";
+import type { ITodo } from "@/types";
 
 export function useTodosData() {
+  const queryClient = useQueryClient();
+
   return useQuery({
     queryKey: QUERY_KEYS.todo.list,
-    queryFn: fetchTodos,
+    // queryFn:fetchTodos,
+    // REFACTOR: 캐시 정규화
+    queryFn: async () => {
+      const todos = await fetchTodos();
+
+      todos.forEach((todo) => {
+        queryClient.setQueryData<ITodo>(QUERY_KEYS.todo.detail(todo.id), todo);
+      });
+      return todos.map((todo) => todo.id);
+    },
   });
 }
